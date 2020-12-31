579b97f204569ec2c7732c3f5507d5934a2472e3
IzMichael <themichael77@outlook.com>
2020-12-31 20:59:35 +1300

    Import Template

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+const header = document.getElementsByTagName('header')[0];
+const navigation0 = document.getElementsByTagName('navigation')[0];
+const navigation1 = document.getElementsByTagName('navigation')[1];
+const wrapper = document.getElementsByTagName('wrapper')[0];
+const footer = document.getElementsByTagName('footer')[0];
+
+window.onload = function () {
+    // Will be run when the entire page loads, including its content
+};
+
